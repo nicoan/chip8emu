a524200fcf5c635e8ec154c2515a720ac393f6fe
Nicolas Antinori <nicolas_antinori@hotmail.com>
2019-03-24 23:05:42 -0300

    Replaced stdout for internal output stream in termion frontend trait

--- src/termion_frontend.rs
+++ src/termion_frontend.rs
@@ -21,38 +21,34 @@ impl TermionFrontend {
 
 impl Frontend for TermionFrontend {
     fn initialize(&mut self) {
-        let mut stdout = stdout().into_raw_mode().unwrap();
         // Clear and hide cursor
-        write!(stdout, "{}", clear::All).unwrap();
-        write!(stdout, "{}", cursor::Hide).unwrap();
+        write!(self.output_stream, "{}", clear::All).unwrap();
+        write!(self.output_stream, "{}", cursor::Hide).unwrap();
 
         // Draw screen box
-        write!(stdout, "{}", clear::All).unwrap();
+        write!(self.output_stream, "{}", clear::All).unwrap();
         // Top row
-        write!(stdout, "{}┌", cursor::Goto(1, 1)).unwrap();
+        write!(self.output_stream, "{}┌", cursor::Goto(1, 1)).unwrap();
         for i in 2..66 {
-            write!(stdout, "{}─", cursor::Goto(i, 1)).unwrap();
+            write!(self.output_stream, "{}─", cursor::Goto(i, 1)).unwrap();
         }
-        write!(stdout, "{}┐", cursor::Goto(66, 1)).unwrap();
+        write!(self.output_stream, "{}┐", cursor::Goto(66, 1)).unwrap();
 
         // Vertical rows
         for i in 2..18 {
-            write!(stdout, "{}│", cursor::Goto(1, i)).unwrap();
-            write!(stdout, "{}│", cursor::Goto(66, i)).unwrap();
+            write!(self.output_stream, "{}│", cursor::Goto(1, i)).unwrap();
+            write!(self.output_stream, "{}│", cursor::Goto(66, i)).unwrap();
         }
 
         // Bottom row
-        write!(stdout, "{}└", cursor::Goto(1, 18)).unwrap();
+        write!(self.output_stream, "{}└", cursor::Goto(1, 18)).unwrap();
         for i in 2..66 {
-            write!(stdout, "{}─", cursor::Goto(i, 18)).unwrap();
+            write!(self.output_stream, "{}─", cursor::Goto(i, 18)).unwrap();
         }
-        write!(stdout, "{}┘", cursor::Goto(66, 18)).unwrap();
-
-        stdout.flush().unwrap();
+        write!(self.output_stream, "{}┘", cursor::Goto(66, 18)).unwrap();
     }
 
     fn draw(&mut self, screen: [[u8; 8]; 32]) {
-        let mut stdout = stdout().into_raw_mode().unwrap();
         const PADDING: u16 = 2;
         for y in (0..16).map(|x| x * 2) {
             for x in 0..8 {
@@ -62,14 +58,14 @@ impl Frontend for TermionFrontend {
                     let x_coord = (x * 8) + i + PADDING;
                     let y_coord: u16 = (y / 2) as u16 + PADDING;
                     match (top_square, bottom_square) {
-                        (true, true) => write!(stdout, "{}█", cursor::Goto(x_coord, y_coord)).unwrap(),
-                        (true, false) => write!(stdout, "{}▀", cursor::Goto(x_coord, y_coord)).unwrap(),
-                        (false, true) => write!(stdout, "{}▄", cursor::Goto(x_coord, y_coord)).unwrap(),
-                        (false, false) => write!(stdout, "{} ", cursor::Goto(x_coord, y_coord)).unwrap()
+                        (true, true) => write!(self.output_stream, "{}█", cursor::Goto(x_coord, y_coord)).unwrap(),
+                        (true, false) => write!(self.output_stream, "{}▀", cursor::Goto(x_coord, y_coord)).unwrap(),
+                        (false, true) => write!(self.output_stream, "{}▄", cursor::Goto(x_coord, y_coord)).unwrap(),
+                        (false, false) => write!(self.output_stream, "{} ", cursor::Goto(x_coord, y_coord)).unwrap()
                     }
                 }
             }
         }
-        stdout.flush().unwrap();
+        self.output_stream.flush().unwrap();
     }
 }
